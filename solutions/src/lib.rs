c8b394b0b17132881181ef85f20c3ca5d02a1b1e
Nikkilae <nikkilae0@gmail.com>
2022-12-14 18:19:29 +0200

    Renaming some files

--- solutions/src/lib.rs
+++ solutions/src/lib.rs
@@ -1,17 +1,17 @@
 use std::error::Error;
 
-pub mod d1;
+pub mod d01;
+pub mod d02;
+pub mod d03;
+pub mod d04;
+pub mod d05;
+pub mod d06;
+pub mod d07;
+pub mod d08;
+pub mod d09;
 pub mod d10;
 pub mod d11;
 pub mod d12;
-pub mod d2;
-pub mod d3;
-pub mod d4;
-pub mod d5;
-pub mod d6;
-pub mod d7;
-pub mod d8;
-pub mod d9;
 
 pub struct Day {
     pub name: String,
@@ -46,15 +46,15 @@ impl Solution {
 
 lazy_static::lazy_static! {
     pub static ref DAYS: Vec<Day> = vec![
-        Day::new("Calorie Counting", Some(Solution::new(include_str!("d1.rs").to_string(), d1::solve_part_1, d1::solve_part_2))),
-        Day::new("Rock Paper Scissors", Some(Solution::new(include_str!("d2.rs").to_string(), d2::solve_part_1, d2::solve_part_2))),
-        Day::new("Rucksack Reorganization", Some(Solution::new(include_str!("d3.rs").to_string(), d3::solve_part_1, d3::solve_part_2))),
-        Day::new("Camp Cleanup", Some(Solution::new(include_str!("d4.rs").to_string(), d4::solve_part_1, d4::solve_part_2))),
-        Day::new("Supply Stacks", Some(Solution::new(include_str!("d5.rs").to_string(), d5::solve_part_1, d5::solve_part_2))),
-        Day::new("Tuning Trouble", Some(Solution::new(include_str!("d6.rs").to_string(), d6::solve_part_1, d6::solve_part_2))),
-        Day::new("No Space Left On Device", Some(Solution::new(include_str!("d7.rs").to_string(), d7::solve_part_1, d7::solve_part_2))),
-        Day::new("Treetop Tree House", Some(Solution::new(include_str!("d8.rs").to_string(), d8::solve_part_1, d8::solve_part_2))),
-        Day::new("Rope Bridge", Some(Solution::new(include_str!("d9.rs").to_string(), d9::solve_part_1, d9::solve_part_2))),
+        Day::new("Calorie Counting", Some(Solution::new(include_str!("d01.rs").to_string(), d01::solve_part_1, d01::solve_part_2))),
+        Day::new("Rock Paper Scissors", Some(Solution::new(include_str!("d02.rs").to_string(), d02::solve_part_1, d02::solve_part_2))),
+        Day::new("Rucksack Reorganization", Some(Solution::new(include_str!("d03.rs").to_string(), d03::solve_part_1, d03::solve_part_2))),
+        Day::new("Camp Cleanup", Some(Solution::new(include_str!("d04.rs").to_string(), d04::solve_part_1, d04::solve_part_2))),
+        Day::new("Supply Stacks", Some(Solution::new(include_str!("d05.rs").to_string(), d05::solve_part_1, d05::solve_part_2))),
+        Day::new("Tuning Trouble", Some(Solution::new(include_str!("d06.rs").to_string(), d06::solve_part_1, d06::solve_part_2))),
+        Day::new("No Space Left On Device", Some(Solution::new(include_str!("d07.rs").to_string(), d07::solve_part_1, d07::solve_part_2))),
+        Day::new("Treetop Tree House", Some(Solution::new(include_str!("d08.rs").to_string(), d08::solve_part_1, d08::solve_part_2))),
+        Day::new("Rope Bridge", Some(Solution::new(include_str!("d09.rs").to_string(), d09::solve_part_1, d09::solve_part_2))),
         Day::new("Cathode-Ray Tube", Some(Solution::new(include_str!("d10.rs").to_string(), d10::solve_part_1, d10::solve_part_2))),
         Day::new("Monkey in the Middle", Some(Solution::new(include_str!("d11.rs").to_string(), d11::solve_part_1, d11::solve_part_2))),
         Day::new("Hill Climbing Algorithm", Some(Solution::new(include_str!("d12.rs").to_string(), d12::solve_part_1, d12::solve_part_2))),
--- solutions/tests/tests.rs
+++ solutions/tests/tests.rs
@@ -12,14 +12,14 @@ fn read_test_file<P: AsRef<Path>>(relative_path: P) -> Result<String, Box<dyn Er
 
 #[test]
 fn day1() -> Result<(), Box<dyn Error>> {
-    assert_eq!(d1::solve_part_1(&read_test_file("d1_test.txt")?)?, "24000");
+    assert_eq!(d01::solve_part_1(&read_test_file("d1_test.txt")?)?, "24000");
     assert_eq!(
-        d1::solve_part_1(&read_test_file("d1_1_real.txt")?)?,
+        d01::solve_part_1(&read_test_file("d1_1_real.txt")?)?,
         "70296"
     );
-    assert_eq!(d1::solve_part_2(&read_test_file("d1_test.txt")?)?, "45000");
+    assert_eq!(d01::solve_part_2(&read_test_file("d1_test.txt")?)?, "45000");
     assert_eq!(
-        d1::solve_part_2(&read_test_file("d1_2_real.txt")?)?,
+        d01::solve_part_2(&read_test_file("d1_2_real.txt")?)?,
         "205381"
     );
     Ok(())
@@ -27,41 +27,47 @@ fn day1() -> Result<(), Box<dyn Error>> {
 
 #[test]
 fn day2() -> Result<(), Box<dyn Error>> {
-    assert_eq!(&d2::solve_part_1(&read_test_file("d2_test.txt")?)?, "15");
-    assert_eq!(&d2::solve_part_1(&read_test_file("d2_real.txt")?)?, "13221");
-    assert_eq!(&d2::solve_part_2(&read_test_file("d2_test.txt")?)?, "12");
-    assert_eq!(&d2::solve_part_2(&read_test_file("d2_real.txt")?)?, "13131");
+    assert_eq!(&d02::solve_part_1(&read_test_file("d2_test.txt")?)?, "15");
+    assert_eq!(
+        &d02::solve_part_1(&read_test_file("d2_real.txt")?)?,
+        "13221"
+    );
+    assert_eq!(&d02::solve_part_2(&read_test_file("d2_test.txt")?)?, "12");
+    assert_eq!(
+        &d02::solve_part_2(&read_test_file("d2_real.txt")?)?,
+        "13131"
+    );
     Ok(())
 }
 
 #[test]
 fn day3() -> Result<(), Box<dyn Error>> {
-    assert_eq!(&d3::solve_part_1(&read_test_file("d3_test.txt")?)?, "157");
-    assert_eq!(&d3::solve_part_1(&read_test_file("d3_real.txt")?)?, "8243");
-    assert_eq!(&d3::solve_part_2(&read_test_file("d3_test.txt")?)?, "70");
-    assert_eq!(&d3::solve_part_2(&read_test_file("d3_real.txt")?)?, "2631");
+    assert_eq!(&d03::solve_part_1(&read_test_file("d3_test.txt")?)?, "157");
+    assert_eq!(&d03::solve_part_1(&read_test_file("d3_real.txt")?)?, "8243");
+    assert_eq!(&d03::solve_part_2(&read_test_file("d3_test.txt")?)?, "70");
+    assert_eq!(&d03::solve_part_2(&read_test_file("d3_real.txt")?)?, "2631");
     Ok(())
 }
 
 #[test]
 fn day4() -> Result<(), Box<dyn Error>> {
-    assert_eq!(&d4::solve_part_1(&read_test_file("d4_test.txt")?)?, "2");
-    assert_eq!(&d4::solve_part_1(&read_test_file("d4_real.txt")?)?, "584");
-    assert_eq!(&d4::solve_part_2(&read_test_file("d4_test.txt")?)?, "4");
-    assert_eq!(&d4::solve_part_2(&read_test_file("d4_real.txt")?)?, "933");
+    assert_eq!(&d04::solve_part_1(&read_test_file("d4_test.txt")?)?, "2");
+    assert_eq!(&d04::solve_part_1(&read_test_file("d4_real.txt")?)?, "584");
+    assert_eq!(&d04::solve_part_2(&read_test_file("d4_test.txt")?)?, "4");
+    assert_eq!(&d04::solve_part_2(&read_test_file("d4_real.txt")?)?, "933");
     Ok(())
 }
 
 #[test]
 fn day5() -> Result<(), Box<dyn Error>> {
-    assert_eq!(&d5::solve_part_1(&read_test_file("d5_test.txt")?)?, "CMZ");
+    assert_eq!(&d05::solve_part_1(&read_test_file("d5_test.txt")?)?, "CMZ");
     assert_eq!(
-        &d5::solve_part_1(&read_test_file("d5_real.txt")?)?,
+        &d05::solve_part_1(&read_test_file("d5_real.txt")?)?,
         "HNSNMTLHQ"
     );
-    assert_eq!(&d5::solve_part_2(&read_test_file("d5_test.txt")?)?, "MCD");
+    assert_eq!(&d05::solve_part_2(&read_test_file("d5_test.txt")?)?, "MCD");
     assert_eq!(
-        &d5::solve_part_2(&read_test_file("d5_real.txt")?)?,
+        &d05::solve_part_2(&read_test_file("d5_real.txt")?)?,
         "RNLFDJMCT"
     );
     Ok(())
@@ -69,26 +75,29 @@ fn day5() -> Result<(), Box<dyn Error>> {
 
 #[test]
 fn day6() -> Result<(), Box<dyn Error>> {
-    assert_eq!(&d6::solve_part_1(&read_test_file("d6_test.txt")?)?, "5");
-    assert_eq!(&d6::solve_part_1(&read_test_file("d6_real.txt")?)?, "1855");
-    assert_eq!(&d6::solve_part_2(&read_test_file("d6_test.txt")?)?, "23");
-    assert_eq!(&d6::solve_part_2(&read_test_file("d6_real.txt")?)?, "3256");
+    assert_eq!(&d06::solve_part_1(&read_test_file("d6_test.txt")?)?, "5");
+    assert_eq!(&d06::solve_part_1(&read_test_file("d6_real.txt")?)?, "1855");
+    assert_eq!(&d06::solve_part_2(&read_test_file("d6_test.txt")?)?, "23");
+    assert_eq!(&d06::solve_part_2(&read_test_file("d6_real.txt")?)?, "3256");
     Ok(())
 }
 
 #[test]
 fn day7() -> Result<(), Box<dyn Error>> {
-    assert_eq!(&d7::solve_part_1(&read_test_file("d7_test.txt")?)?, "95437");
     assert_eq!(
-        &d7::solve_part_1(&read_test_file("d7_real.txt")?)?,
+        &d07::solve_part_1(&read_test_file("d7_test.txt")?)?,
+        "95437"
+    );
+    assert_eq!(
+        &d07::solve_part_1(&read_test_file("d7_real.txt")?)?,
         "1749646"
     );
     assert_eq!(
-        &d7::solve_part_2(&read_test_file("d7_test.txt")?)?,
+        &d07::solve_part_2(&read_test_file("d7_test.txt")?)?,
         "24933642"
     );
     assert_eq!(
-        &d7::solve_part_2(&read_test_file("d7_real.txt")?)?,
+        &d07::solve_part_2(&read_test_file("d7_real.txt")?)?,
         "1498966"
     );
     Ok(())
@@ -96,11 +105,11 @@ fn day7() -> Result<(), Box<dyn Error>> {
 
 #[test]
 fn day8() -> Result<(), Box<dyn Error>> {
-    assert_eq!(&d8::solve_part_1(&read_test_file("d8_test.txt")?)?, "21");
-    assert_eq!(&d8::solve_part_1(&read_test_file("d8_real.txt")?)?, "1805");
-    assert_eq!(&d8::solve_part_2(&read_test_file("d8_test.txt")?)?, "8");
+    assert_eq!(&d08::solve_part_1(&read_test_file("d8_test.txt")?)?, "21");
+    assert_eq!(&d08::solve_part_1(&read_test_file("d8_real.txt")?)?, "1805");
+    assert_eq!(&d08::solve_part_2(&read_test_file("d8_test.txt")?)?, "8");
     assert_eq!(
-        &d8::solve_part_2(&read_test_file("d8_real.txt")?)?,
+        &d08::solve_part_2(&read_test_file("d8_real.txt")?)?,
         "444528"
     );
     Ok(())
@@ -108,10 +117,10 @@ fn day8() -> Result<(), Box<dyn Error>> {
 
 #[test]
 fn day9() -> Result<(), Box<dyn Error>> {
-    assert_eq!(&d9::solve_part_1(&read_test_file("d9_1_test.txt")?)?, "13");
-    assert_eq!(&d9::solve_part_1(&read_test_file("d9_real.txt")?)?, "5907");
-    assert_eq!(&d9::solve_part_2(&read_test_file("d9_2_test.txt")?)?, "36");
-    assert_eq!(&d9::solve_part_2(&read_test_file("d9_real.txt")?)?, "2303");
+    assert_eq!(&d09::solve_part_1(&read_test_file("d9_1_test.txt")?)?, "13");
+    assert_eq!(&d09::solve_part_1(&read_test_file("d9_real.txt")?)?, "5907");
+    assert_eq!(&d09::solve_part_2(&read_test_file("d9_2_test.txt")?)?, "36");
+    assert_eq!(&d09::solve_part_2(&read_test_file("d9_real.txt")?)?, "2303");
     Ok(())
 }
 
